96a7430768b6ef44ae4a1e20c851c1af5da7fb25
Chigo-07 <nkemerechigozirim14@gmail.com>
2026-01-16 06:30:26 +0100

    Fix pickle unpacking and scikit-learn version

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 # Load model
 with open("model.h5", "rb") as f:
-    model, scaler = pickle.load(f)
+    # Grab the model and scaler, and ignore anything else in the file
+    data = pickle.load(f)
+    model = data[0]
+    scaler = data[1]
+    
 
 @app.route("/", methods=["GET", "POST"])
 def home():
